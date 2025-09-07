59b575e6cb1e4e13b82751117de9676b0c4d20a9
adynah <t.barimalaa@gmail.com>
2025-09-07 02:02:19 +0100

    Add views and JWT authentication routes (register, login, refresh, profile)

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class UserView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
